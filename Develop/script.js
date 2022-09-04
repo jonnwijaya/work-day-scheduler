92074659dfa4cda8d7bdef97e1183d8e8bcf68ad
Jonathan <jonathanwijayan@gmail.com>
2022-09-04 21:04:48 +1000

    Added timeblock to the html file

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,11 @@
 var today = moment().format("dddd, MMMM Do");
+
 $("#currentDay").text(today);
+
+function editText(event) {
+    event.preventDefault();
+
+    var text = $('input[name="text-input"]').val();
+
+
+}
