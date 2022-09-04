bf531655ff92f22982f77441a72b5e9358b8fdf2
Jonathan <jonathanwijayan@gmail.com>
2022-09-04 23:44:00 +1000

    Added the function to the javascript file, finished the README file, and added the css file

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,43 @@ var today = moment().format("dddd, MMMM Do");
 
 $("#currentDay").text(today);
 
-function editText(event) {
-    event.preventDefault();
+$(document).ready(function () {
 
-    var text = $('input[name="text-input"]').val();
+    $('.saveBtn').on('click', function () {
+        var desc = $(this).siblings('.description').val();
+        var hour = $(this).parent().attr('id');
 
+        localStorage.setItem(hour, desc);
+    });
 
-}
+    function timeOfDay() {
+        var currentTime = moment().hours();
+
+        $('.time-block').each(function () {
+            var timeBlockTime = parseInt($(this).attr('id').split('-')[1]);
+
+            if (timeBlockTime < currentTime) {
+                $(this).addClass('past');
+            } else if (timeBlockTime === currentTime) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    timeOfDay();
+
+    $('#9 .description').val(localStorage.getItem('9'));
+    $('#10 .description').val(localStorage.getItem('10'));
+    $('#11 .description').val(localStorage.getItem('11'));
+    $('#12 .description').val(localStorage.getItem('12'));
+    $('#13 .description').val(localStorage.getItem('13'));
+    $('#14 .description').val(localStorage.getItem('14'));
+    $('#15 .description').val(localStorage.getItem('15'));
+    $('#16 .description').val(localStorage.getItem('16'));
+    $('#17 .description').val(localStorage.getItem('17'));
+});
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # work-day-scheduler
+
+## Description
+- The motivation behind this project is to create a Work Day Scheduler webpage
+- The changes made in the html were: added Timeblock and CDNs
+- Added a javascript to make the content savable, added moment.js, and create the site logic
+- Added css file to color code the timeblock based on past (grey), present (red), future (green)
+
+## Installation
+
+1. Open the link
+2. Input text in the textbox
+3. Click the save button on the right side of the timeblock
+
+## Usage
+
+Work Day Scheduler webpage
+
+Link: https://jonnwijaya.github.io/horiseon-webpage-refactoring/
+
+![Screenshot of the site](Assets/images/demo.gif)
+
+## Credits
+
+The site html starter code was created by the USYD team
